93e94f6def716fc08a51dd1fe8e3ae4fbe826139
Pratik Sengupta <pratik006@gmail.com>
2018-05-23 12:50:54 +0530

    added starter script

--- client/starter.sh
+++ client/starter.sh
@@ -2,4 +2,4 @@ cd ../api
 mvn clean install
 cd ../client
 mvn clean package
-java -Dlog4j.configuration="file:/log4j.properties" -jar uci-client-jar-with-dependencies.jar
+java -Dlog4j.configuration="file:./log4j.properties" -jar uci-client-jar-with-dependencies.jar
